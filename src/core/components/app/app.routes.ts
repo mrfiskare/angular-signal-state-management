23116cb14819548e9109e8b7424496bea6b8ee96
Roland Halász <roland.halasz@outlook.com>
2024-09-11 16:37:20 +0200

    add more routes

--- src/core/components/app/app.routes.ts
+++ src/core/components/app/app.routes.ts
@@ -3,6 +3,30 @@ import { HomeComponent } from '../../../pages/home/home.component';
 
 export const routes: Routes = [
   { path: 'home', title: 'Home', component: HomeComponent },
+  {
+    path: 'new-order',
+    title: 'New Order',
+    loadComponent: () =>
+      import('../../../pages/order-items/order-items.component').then(
+        (m) => m.OrderItemsComponent
+      )
+  },
+  {
+    path: 'my-orders',
+    title: 'My Orders',
+    loadComponent: () =>
+      import('../../../pages/my-orders/my-orders.component').then(
+        (m) => m.MyOrdersComponent
+      )
+  },
+  {
+    path: 'summary',
+    title: 'Summary',
+    loadComponent: () =>
+      import('../../../pages/summary/summary.component').then(
+        (m) => m.SummaryComponent
+      )
+  },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
