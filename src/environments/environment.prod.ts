d1de902c6ac4b239183a37b3954ac253c4fa04b9
Roland Halász <roland.halasz@outlook.com>
2024-09-11 13:34:23 +0200

    setup environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  enableDebug: false
+};
+
+// TODO: Specify production value!
+export const keycloakConfig = {
+  url: 'http://localhost:8080',
+  realm: 'master',
+  clientId: 'UTK-client'
+};
+
+// TODO: Specify production value!
+export const logoutRedirectUri = 'http://localhost:4200';
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  enableDebug: true
+};
+
+export const keycloakConfig = {
+  url: 'http://localhost:8080',
+  realm: 'master',
+  clientId: 'UTK-client'
+};
+
+export const logoutRedirectUri = 'http://localhost:4200';
