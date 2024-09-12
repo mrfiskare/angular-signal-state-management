920f391b0dcfee9a49d2294fce8025591a1eb18a
Roland Halász <roland.halasz@outlook.com>
2024-09-12 17:45:21 +0200

    add reset functionality

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { Router } from '@angular/router';
+import { StateService } from '../../core/services/state.service';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,10 @@ import { Router } from '@angular/router';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-  constructor(private router: Router) {}
+  constructor(
+    private router: Router,
+    protected state: StateService
+  ) {}
 
   navigateTo(route: string): void {
     void this.router.navigate([route]);
